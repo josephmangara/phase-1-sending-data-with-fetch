d9bf39817ae0e95989a6e7a499816b6d603e78b9
josephmangara <josephmangara2@gmail.com>
2023-10-18 23:33:53 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function submitData(name, email){
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        }),
+    };
+    
+    fetch('http://localhost:3000/users', configurationObject)
+    .then(response => response.json())
+    .then(function(object){console.log(object);
+    });
+}
+
+try {submitData}
+catch(error){alert('Unauthorized Access');
+console.error('Error message:', error.message);
+};
